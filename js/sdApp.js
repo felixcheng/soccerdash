131988718967a52c6550bc62553e283f17ed0156
felixcheng <chengfelix@gmail.com>
2014-02-13 14:27:40 -0800

    1st framework

---
+++ js/sdApp.js
@@ -0,0 +1,17 @@
+var soccerDashApp = angular.module('soccerDashApp', 
+	['ngRoute', 'ngResource', 'soccerDashControllers', 'soccerDashServices', "firebase"]);
+
+soccerDashApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+		$routeProvider.
+			when('/league', {
+				templateUrl: 'partials/leaguetbl.html',
+				controller: 'LeagueTblCtrl'
+			}).
+			// when('/portfo', {
+			// 	templateUrl: 'partials/portfolio.html',
+			// 	controller: 'PortfolioCtrl'
+			// }).
+			otherwise({
+				redirectTo: '/index'
+			})
+}]);
