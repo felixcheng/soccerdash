bfdca26b309174664050114cd31c6dd9612d23b5
Ben Stone <benjamin.v.stone@gmail.com>
2014-02-13 18:47:33 -0800

    Added HeaderCtrl controller for home.html template. Created team hash to populate navigation menu

--- js/sdControllers.js
+++ js/sdControllers.js
@@ -23,9 +23,29 @@ soccerDashControllers.controller("LeagueTblCtrl", ["$scope", "$http",
 
 }]);
 
-soccerDashControllers.controller("HeaderCtrl", ["$scope",
-  function($scope){
-    $scope.team = {
-      name: "Liverpool FC"
-    }
+soccerDashControllers.controller("HeaderCtrl", ["$scope", function($scope){
+  // Array of team objects 
+  $scope.teams = [ 
+    { shortName: "arsenal", fullName: 'Arsenal' },
+    { shortName: "aston-villa", fullName: 'Aston Villa' },
+    { shortName: "cardiff-city", fullName: 'Cardiff City' },
+    { shortName: "chelsea", fullName: 'Chelsea' },
+    { shortName: "crystal-palace", fullName: 'Crystal Palace' },
+    { shortName: "everton", fullName: 'Everton' }, 
+    { shortName: "fulham", fullName: 'Fulham' },
+    { shortName: "hull-city", fullName: 'Hull City' },
+    { shortName: "liverpool", fullName: 'Liverpool' }, 
+    { shortName: "manchester-city", fullName: 'Manchester City' }, 
+    { shortName: "manchester-united", fullName: 'Manchester United' }, 
+    { shortName: "newcastle-united", fullName: 'Newcastle United' },
+    { shortName: "norwich-city", fullName: 'Norwich City' }, 
+    { shortName: "southampton", fullName: 'Southampton' }, 
+    { shortName: "stoke-city", fullName: 'Stoke City' }, 
+    { shortName: "sunderland", fullName: 'Sunderland' }, 
+    { shortName: "swansea-city", fullName: 'Swansea City' }, 
+    { shortName: "tottenham-hotspur", fullName: 'Tottenham Hotspur' }, 
+    { shortName: "west-bromwich-albion", fullName: 'West Bromwich Albion' }, 
+    { shortName: "west-ham-united", fullName: 'West Ham United' }
+  ];
+
 }]);
