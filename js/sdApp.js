130ba65f87b1d12849c54b53e4157c876c40a582
David Mansy <david.mansy@icloud.com>
2014-02-13 20:02:06 -0800

    Ability to log in via Facebook: done. Firebase back-end connected to the app: done. Firebase hosting: done.

--- js/sdApp.js
+++ js/sdApp.js
@@ -1,17 +1,25 @@
+//Main app
 var soccerDashApp = angular.module('soccerDashApp', 
-	['ngRoute', 'ngResource', 'soccerDashControllers', 'soccerDashServices', /*"firebase"*/]);
+  ['ngRoute', 'ngResource', 'soccerDashControllers', 'soccerDashServices', 'firebase']);
 
+//Routes configuration
 soccerDashApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-		$routeProvider.
-			when('/league', {
-				templateUrl: 'partials/leaguetbl.html',
-				controller: 'LeagueTblCtrl'
-			}).
-			// when('/portfo', {
-			// 	templateUrl: 'partials/portfolio.html',
-			// 	controller: 'PortfolioCtrl'
-			// }).
-			otherwise({
-				redirectTo: '/index'
-			})
+  $routeProvider
+  .when('/', {
+    templateUrl: "partials/home.html",
+    controller: "HomeController"
+  })
+  .when('/login', {
+    templateUrl: "partials/login.html",
+    controller: "LoginController"
+  })
+  .when('/league', {
+    templateUrl: 'partials/leaguetbl.html',
+    controller: 'LeagueTblCtrl'
+  })
+  // when('/portfo', {
+  //  templateUrl: 'partials/portfolio.html',
+  //  controller: 'PortfolioCtrl'
+  // }).
+  .otherwise({redirect_to: '/login'})
 }]);
--- js/sdControllers.js
+++ js/sdControllers.js
@@ -1,14 +1,59 @@
-var soccerDashControllers  = angular.module('soccerDashControllers', ['soccerDashServices', /*'firebase'*/]);
+var soccerDashControllers = angular.module('soccerDashControllers', ['soccerDashServices', 'firebase']);
+
+soccerDashControllers.controller('IndexController',
+  ['$scope', '$location', '$firebaseSimpleLogin', '$firebase',
+    function($scope, $location, $firebaseSimpleLogin, $firebase) {
+
+    //Firebase members data collection
+    var dataRef = new Firebase('https://soccerdashboard.firebaseio.com/members');
+
+    //Firebase/Github Authentication
+    $scope.loginObj = $firebaseSimpleLogin(dataRef);
+
+    //Listening to login
+    $scope.$on("$firebaseSimpleLogin:login", function(evt, user) {
+      console.log("User " + user.id + " successfully logged in!");
+      $location.path("/"); //When a user is logged in, redirect him to the '/''
+      //Add user to the list of members, will not add the user if it already exists because same key
+      $scope.members = $firebase(dataRef);
+      $scope.members[user['id']] = user;
+      $scope.members.$save(user['id']);
+      //Add user to the scope, maybe it could be helpful?
+      $scope.user = user;
+    });
+
+    //Listening to logout
+    $scope.$on("$firebaseSimpleLogin:logout", function(evt) {
+      console.log("User logged out!");
+      $location.path("/login"); //When a user is logged out, redirect him to '/login'
+    });
+
+    //Listening to authentication error
+    $scope.$on("$firebaseSimpleLogin:error", function(err) {
+      console.log("Authentication error: " + err);
+    });
+
+}]);
 
 soccerDashControllers.controller("LeagueTblCtrl", ["$scope", "$http",
-	function($scope, $http){
-
+  function($scope, $http){
 
 }]);
 
 soccerDashControllers.controller("HeaderCtrl", ["$scope",
   function($scope){
+    //TO DO: The selected team should already be in the $scope, loaded by the home controller
     $scope.team = {
       name: "Liverpool FC"
-    }
+    };
+}]);
+
+soccerDashControllers.controller("LoginController", ["$scope",
+  function($scope){
+
+}]);
+
+soccerDashControllers.controller("HomeController", ["$scope",
+  function($scope){
+
 }]);
