1f709de315bd45ed6fab7cb1b4b0a907e1c7df8f
Ben Stone <benjamin.v.stone@gmail.com>
2014-02-27 15:17:20 -0800

    Moved formatDate function to helper function file for better organization

--- helpers/helperFunction.js
+++ helpers/helperFunction.js
@@ -12,3 +12,28 @@ var changeOrdinal = function(scope){
   }
   console.log('ord', scope.currentTeam.positionOr )
 }
+
+//Format the ISO dates received by the API
+var formatDate = function(isoString) {
+  var date = new Date(isoString);
+  var months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+                  
+  var dayNum = function(day) {
+    var append; 
+    if(day == 1 || day === 21 || day === 31) {
+      append = 'st';
+    }
+    else if(day === 2 || day === 22) {
+      append = 'nd';
+    }
+    else if(day === 3 || day === 23) {
+      append = 'rd';
+    }
+    else {
+      append = 'th';
+    }
+    return day+append;
+  }
+  return days[date.getDay()] + " " + dayNum(date.getDate()) + " " + months[date.getMonth()]; 
+};
