88605240d7381e8f1fdc82edeca6eb3bb9e63c1b
David Mansy <david.mansy@icloud.com>
2014-02-27 11:43:34 -0800

    Functional refactoring top scorers

--- js/sdControllers.js
+++ js/sdControllers.js
@@ -284,25 +284,25 @@ soccerDashControllers.controller('ModalCtrl', ['$scope',
 }]);
 
 // Team Top Scorers Controller
-soccerDashControllers.controller('TeamTopScorersController', ['$scope', 'statsfcService',
-  function($scope, statsfcService) {
+// soccerDashControllers.controller('TeamTopScorersController', ['$scope', 'statsfcService',
+//   function($scope, statsfcService) {
 
-  $scope.$watch('currentTeam', function(newVal, oldVal, scope) {
-    if(newVal) {
-      $scope.showGoal = false;
+//   $scope.$watch('currentTeam', function(newVal, oldVal, scope) {
+//     if(newVal) {
+//       $scope.showGoal = false;
 
-      statsfcService.getTeamTopScorers(newVal.teampath)
-      .then(function(data) {
-        $scope.goalData = [];
-        for(var i = 0; i < 8; i++) { 
-          $scope.goalData.push(data[i]);
-        }
-        $scope.showGoal = true;
-      });
-    }
-  });
+//       statsfcService.getTeamTopScorers(newVal.teampath)
+//       .then(function(data) {
+//         $scope.goalData = [];
+//         for(var i = 0; i < 8; i++) { 
+//           $scope.goalData.push(data[i]);
+//         }
+//         $scope.showGoal = true;
+//       });
+//     }
+//   });
 
-}]);
+// }]);
 
 var changeOrdinal = function(scope){
   if (typeof scope.currentTeam.position ===  'number'){
---
+++ topscorers/TeamTopScorersControllerModule.js
@@ -0,0 +1,22 @@
+angular.module('teamTopScorersControllerModule', ['soccerDashServices'])
+
+.controller('TeamTopScorersController',
+  ['$scope', 'statsfcService',
+    function($scope, statsfcService) {
+
+  $scope.$watch('currentTeam', function(newVal, oldVal, scope) {
+    if(newVal) {
+      $scope.showGoal = false;
+
+      statsfcService.getTeamTopScorers(newVal.teampath)
+      .then(function(data) {
+        $scope.goalData = [];
+        for(var i = 0; i < 8; i++) { 
+          $scope.goalData.push(data[i]);
+        }
+        $scope.showGoal = true;
+      });
+    }
+  });
+
+}]);
