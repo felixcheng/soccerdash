ea71fc0f69f678974b98cbdf39fdaa5d01d2973e
David Mansy <david.mansy@icloud.com>
2014-02-28 11:43:38 -0800

    Adding the hack reactor banner

--- main/navAnimations.js
+++ main/navAnimations.js
@@ -10,4 +10,12 @@ $(document).ready(function() {
   $('body').on('click', '.close', function() {
     $('.menu').animate({left: '-31%', opacity: '0'}, 1000);
   })
+
+  $('body').append(
+  '<a href="http://hackreactor.com"> \
+  <img style="position: fixed; top: 0; right: 0; border: 0;" \
+  src="http://i.imgur.com/x86kKmF.png" \
+  alt="Built at Hack Reactor"> \
+  </a>');
+
 })
